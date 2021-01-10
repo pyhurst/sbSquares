b2fcb3e39e614064be7cefb848240bf2fe2f6777
Phil Hurst <youngphil9@gmail.com>
2021-01-10 12:50:12 -0600

    added react pages, finished oauth flow

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Landing from '../pages/Landing/Landing';
 import UserProfile from '../pages/User_Profile/User_Profile';
 import Header from './Header/Header';
 
-const App = () => {
-    return (
-        <div className='container'>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route exact path="/userprofile" component={UserProfile} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route exact path="/userprofile" component={UserProfile} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
