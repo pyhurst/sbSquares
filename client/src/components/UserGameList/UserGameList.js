f30ebca413108b85814092678359cb85ebb04a3e
Phil Hurst <youngphil9@gmail.com>
2021-01-11 15:14:52 -0600

    user profile game links

--- client/src/components/UserGameList/UserGameList.js
+++ client/src/components/UserGameList/UserGameList.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserGameList = props => {
     return props.userGames.map(game => {
         return (
-            <li key={game._id} >
-                <a>{game._id}</a>
+            <li key={game._id}>
+                <Link to={`/game/${game._id}`} id={game._id} >{game._id}</Link>
             </li>
         )
     })
