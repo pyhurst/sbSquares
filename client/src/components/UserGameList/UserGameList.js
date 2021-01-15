0e3201546699af92be4140c775a43f15f02693be
Philip Hurst <youngphil9@gmail.com>
2021-01-15 09:19:08 -0600

    started copy link for userlist

--- client/src/components/UserGameList/UserGameList.js
+++ client/src/components/UserGameList/UserGameList.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const UserGameList = props => {
+    const copyLink = () => {
+
+    }
+
     return props.userGames.map(game => {
         return (
             <li key={game._id}>
                 <Link to={`/game/${game._id}`} id={game._id} >{game._id}</Link>
+                <button type='button' className='btn btn-success' onClick={copyLink}>Copy</button>
             </li>
         )
     })
