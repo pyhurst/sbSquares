85f237ff5401aebf33d09dc3a754252f84550840
Philip Hurst <youngphil9@gmail.com>
2021-01-15 19:52:51 -0600

    landing, userprofile, desktop/iphone styling changes

--- client/src/components/UserGameList/UserGameList.js
+++ client/src/components/UserGameList/UserGameList.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import './UserGameList.css';
 
 const UserGameList = props => {
-    const copyLink = (e) => {
+    const copyLink = e => {
         navigator.clipboard.writeText(`https://sb-sqaures.herokuapp.com/game/${e.target.id}`)
     }
 
@@ -11,7 +11,7 @@ const UserGameList = props => {
         return (
             <li className='user-list-item' key={game._id}>
                 <Link to={`/game/${game._id}`} id={game._id} >{game._id}</Link>
-                <button type='button' className='copybtn btn btn-outline-success btn-sm' onClick={copyLink} id={game._id}>Copy</button>
+                <button type='button' className='copybtn btn btn-outline-success btn-sm' onClick={copyLink} id={game._id}>Copy Link</button>
             </li>
         )
     })
