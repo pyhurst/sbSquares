b4d4b0df373f28f0a2685cdd2f7abaf3dba6d991
Philip Hurst <youngphil9@gmail.com>
2021-01-16 15:32:58 -0600

    modal component added, delete modal confirmation added

--- client/src/pages/User_Profile/User_Profile.js
+++ client/src/pages/User_Profile/User_Profile.js
@@ -3,10 +3,13 @@ import { connect } from 'react-redux';
 import API from '../../utils/API';
 import UserGameList from '../../components/UserGameList/UserGameList';
 import Header from '../../components/Header/Header';
+import Modal from '../../components/Modal/Modal';
 import './User_Profile.css';
 
 const UserProfile = (props) => {
     const [userGames, setUserGames] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedGameId, setSelectedGameId] = useState('');
 
     useEffect(() => {
         getUserGames();
@@ -31,14 +34,20 @@ const UserProfile = (props) => {
     }
 
     const deleteGame = e => {
-        API.deleteGame(e.target.id)
+        API.deleteGame(selectedGameId)
             .then(() => {
+                setIsOpen(false)
                 API.getUserGames(props.auth._id)
                 .then(result => {
                     console.log(result.data)
                     setUserGames(result.data)
                 })
             })
+    }
+
+    const openConfirmation = e => {
+        setSelectedGameId(e.target.id);
+        setIsOpen(true);
     }
 
     if (!props.auth) {
@@ -51,8 +60,9 @@ const UserProfile = (props) => {
                     <h1>User Profile</h1>
                     <h4 id='created-games-title'>Created Games</h4>
                     <div className='created-games'>
+                        <Modal open={isOpen} gameId={selectedGameId} deleteGame={deleteGame} onClose={() => setIsOpen(false)}>Are you sure you want to permanently delete this game?</Modal>
                         <ul>
-                            <UserGameList userGames={userGames} deleteGame={deleteGame} />
+                            <UserGameList userGames={userGames} openConfirmation={openConfirmation} />
                         </ul>
                     </div>
                     <button type='button' className='btn btn-success createbtn' onClick={createGame}>Create Game</button>
