c8fc69ef32462250a02c40b2b211f3aa337da656
Schwyn Francis <schwynf@gmail.com>
2021-01-20 07:12:44 -0700

    maybe

--- client/src/pages/Game/Game.js
+++ client/src/pages/Game/Game.js
@@ -29,6 +29,7 @@ const Game = (props) => {
     // const [finish, setFinish] = useState(true);
     const [xArray, setXarray] = useState(["", "", "", "", "", "", "", "", "", ""]);
     const [yArray, setYarray] = useState(["", "", "", "", "", "", "", "", "", ""]);
+    const [blackNumbers, setBlackNumbers] = useState(true)
 
     let flip = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 
@@ -197,7 +198,7 @@ const Game = (props) => {
                     </div>
                     <div className="col-2 col-md-2"></div>
                 </div>
-                <div className="row mb-5">
+                {/* <div className="row mb-5">
                     <div className="col-12">
                         <div>
                             <h5 id="h0" className="h-numbers">{xArray[0]}</h5><h5 id="h1" className="h-numbers" >{xArray[1]}</h5>
@@ -207,14 +208,15 @@ const Game = (props) => {
                             <h5 id="h8" className="h-numbers">{xArray[8]}</h5><h5 id="h9" className="h-numbers">{xArray[9]}</h5>
                         </div>
                     </div>
-                </div>
+                </div> */}
                 <div className="row">
                     <div className="col-2 col-md-3 justify-content-right">
                         <div className="row">
                             <div className="col-10">
                             </div>
-                            <div className="col-2">
+                            <div className="col-2 mt-5">
                                 <div>
+                                    
                                     <h2 className="text-right y-row">{yArray[0]}</h2>
                                     <h2 className="text-right y-row">{yArray[1]}</h2>
                                     <h2 className="text-right y-row">{yArray[2]}</h2>
@@ -230,18 +232,23 @@ const Game = (props) => {
                         </div>
                     </div>
                     <div className="col-10 col-md-8">
-                        {/* <div className="row">
-                            <div className="col-12 text-left">
-                                <div>
-                                    <h5 id="h0" className="h-numbers">{xArray[0]}</h5><h5 id="h1" className="h-numbers" >{xArray[1]}</h5>
-                                    <h5 id="h2" className="h-numbers">{xArray[2]}</h5><h5 id="h3" className="h-numbers">{xArray[3]}</h5>
-                                    <h5 id="h4" className="h-numbers">{xArray[4]}</h5><h5 id="h5" className="h-numbers">{xArray[5]}</h5>
-                                    <h5 id="h6" className="h-numbers">{xArray[6]}</h5><h5 id="h7" className="h-numbers">{xArray[7]}</h5>
-                                    <h5 id="h8" className="h-numbers">{xArray[8]}</h5><h5 id="h9" className="h-numbers">{xArray[9]}</h5>
-                                </div>
-                            </div>
-                        </div> */}
+
                         <div className="row">
+                            {rowLength.map((user, i) => (
+                                <div className="col-1" key={i}>
+                                    <Square squareId="1-2" id={i} blackNumbers={blackNumbers} adminEdit={adminEdit} color={squares[i].color} modalAdmin={modalAdmin} flipFunciton={flipFunction} isFlipped={flipStatus[i]} active={squares[i].active}>
+                                        {xArray[i]}
+                                    </Square>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="row">
+                            {/* <div className="col-1" key={0}>
+                                <Square squareId="1-2" id={0} blackNumbers={blackNumbers} adminEdit={adminEdit} color={squares[0].color} modalAdmin={modalAdmin} flipFunciton={flipFunction} isFlipped={flipStatus[0]} active={squares[0].active}>
+                                    {xArray[0]}
+                                </Square>
+                            </div> */}
                             {rowLength.map((user, i) => (
                                 <div className="col-1" key={i}>
                                     <Square squareId="1-2" id={i} adminEdit={adminEdit} color={squares[i].color} modalAdmin={modalAdmin} flipFunciton={flipFunction} isFlipped={flipStatus[i]} active={squares[i].active}>
